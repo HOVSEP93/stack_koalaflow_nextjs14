a7f4334c258fd61058c3830b0936d1e6547e1345
Hovsep93 <hovsepantranig@gmail.com>
2024-05-14 09:35:09 +1000

    apply tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +12,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
